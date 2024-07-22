a77f5ab196f1dcfeb2d3c18d6dfd71a245ad8222
hpham474 <hpham474@gmail.com>
2024-07-21 21:58:16 -0500

    adjust node class to be able to hold keys

--- Node.mjs
+++ Node.mjs
@@ -1,15 +1,23 @@
 class Node {
+    key;
     value;
     nextNode;
     constructor() {
+        this.key = null;
         this.value = null;
         this.nextNode = null;
+    }
+    get key () {
+        return this.key;
     }
     get value () {
         return this.value;
     }
     get nextNode () {
         return this.nextNode;
+    }
+    set key (key) {
+        this.key = key;
     }
     set value (val) {
         this.value = val;
